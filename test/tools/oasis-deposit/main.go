305a122aa414eeefa16918052de35a15abd765c1
Matevž Jekovec <matevz.jekovec@oasislabs.com>
2021-10-22 11:39:00 +0200

    oasis-deposit: Increase timeout to 120s

--- test/tools/oasis-deposit/main.go
+++ test/tools/oasis-deposit/main.go
@@ -128,7 +128,7 @@ func main() {
 	acc := accounts.NewV1(rtc)
 	consAcc := consAccClient.NewV1(rtc)
 
-	ctx, cancelFn := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancelFn := context.WithTimeout(context.Background(), 120*time.Second)
 	defer cancelFn()
 
 	ba := types.NewBaseUnits(*quantity.NewFromUint64(*amount), types.NativeDenomination)
